9105f46a4a5f5db5d748b63e510daa5a5dd27530
Maria The Best <me@gmail.com>
2024-04-01 22:12:51 +0200

    First input

--- BattleShip.py
+++ BattleShip.py
@@ -10,3 +10,6 @@ class ship:
 		self.location = location
 		self.size = size
 
+name_player = input("Welcome to Battle Ship! Please, enter your name")
+player1 = player(name_player)
+print("Your name is "player1.name)
